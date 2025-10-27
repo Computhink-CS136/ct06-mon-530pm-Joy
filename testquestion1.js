e445f693645bf03a434cd9af24627f64a60cd390
Computhink-CS136 <cs136@computhink.com.sg>
2025-10-27 10:35:37 +0000

    End Test

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,15 @@
+let xpos;
+let ypos;
 
+function setup () {
+    createCanvas(600, 400);
+    background("lightblue");
+}
+function draw () {
+    background("lightblue")
+    xpos = 300;
+    ypos = 200;
+    fill("limegreen");
+    circle(xpos, ypos, 50);
+    
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,18 @@
+let pokemons = ["Pikachu", "Raichu", "Bulbasaur", "Mewtwo", "Charizard"];
+let yPos;
 
+function setup(){
+    createCanvas(800, 500);
+    background("black")
+    for (let index = 0; index < pokemons.length; index++) {
+        textSize(20);
+        textAlign(CENTER, CENTER);
+        fill("pink");
+        text(pokemons[0], 400, 150);
+        text(pokemons[1], 400, 200);
+        text(pokemons[2], 400, 250);
+        text(pokemons[3], 400, 300);
+        text(pokemons[4], 400, 350);
+    } 
+    yPos = 100;
+}
